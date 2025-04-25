3eaaa53965480628f77559192025fae915601cd2
exeloz <christophercoulombe@gmail.com>
2025-04-25 16:12:06 -0400

    Security : Password Hashing and ConfigurationService

---
+++ LoginApp/Utils/Services/Interfaces/IConfigurationService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginApp.Utils.Services.Interfaces
+{
+    public interface IConfigurationService
+    {
+        string GetDbPath();
+
+        string GetDefaultAdminUserName();
+
+        string GetDefaultAdminPassword();
+    }
+}
