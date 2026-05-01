08ad768b84e03a5c653a866b45cff76f479c918c
exeloz <christophercoulombe@gmail.com>
2026-05-01 12:46:56 -0400

    feat(security): Ajout des gestions d'erreurs

--- LoginApp/App.xaml.cs
+++ LoginApp/App.xaml.cs
@@ -59,7 +59,16 @@ namespace LoginApp
 
             var mainWindow = _serviceProvider.GetRequiredService<MainWindow>();
             mainWindow.Show();
+
+            this.DispatcherUnhandledException += (sender, args) =>
+            {
+                MessageBox.Show($"Une erreur non gérée s'est produite.", "Erreur", MessageBoxButton.OK, MessageBoxImage.Error);
+                args.Handled = true;
+            };
+
             base.OnStartup(e);
         }
+
+
     }
 }
